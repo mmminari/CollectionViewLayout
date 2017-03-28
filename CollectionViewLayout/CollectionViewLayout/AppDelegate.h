2de4fa1fd4f03e40a8f38d79e01b256dea29b5d3
mmminari <mmminari@smtown.com>
2017-03-28 18:02:11 +0900

    first commit

---
+++ CollectionViewLayout/CollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewLayout
+//
+//  Created by 김민아 on 2017. 3. 28..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
