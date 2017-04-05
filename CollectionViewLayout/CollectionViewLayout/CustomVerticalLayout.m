82a052558d42221f9a947cbfbf5ccd824aa23e5f
mmminari <mmminari@smtown.com>
2017-04-05 16:51:11 +0900

    계산하는 부분 변경

--- CollectionViewLayout/CollectionViewLayout/CustomVerticalLayout.m
+++ CollectionViewLayout/CollectionViewLayout/CustomVerticalLayout.m
@@ -19,7 +19,8 @@
 
 @interface CustomVerticalLayout ()
 
-@property (assign, nonatomic) CGFloat contentHeight;
+@property (strong, nonatomic) NSArray *attributes;
+@property (assign, nonatomic) CGFloat heightOfContentView;
 
 @end
 
@@ -29,25 +30,46 @@
 {
     [super prepareLayout];
     
+    NSLog(@"prepareLayout");
+    
+    // layoutAttributesForElementsInRect에서 array정보를 구할 경우
+    // 함수가 호출될 때마다 계산이 이루어짐
+    // prepareLayout은 처음 한번 호출된 후 invalidateLayout을 호출해야
+    // 재 호출됨으로 이때 초기에 필요한 정보들을 계산해 주는 것이 좋음
+    self.attributes = [self makeAttributes];
+    
+    self.heightOfContentView = [self calculateHeightOfContentView];
+    
 }
 
 - (CGSize)collectionViewContentSize
 {
-    //셀을 그릴 수 있는 영역을 지정
-    CGSize result = CGSizeMake(DEVICE_WIDTH, self.contentHeight);
+    // 셀을 그릴 수 있는 영역을 지정
+    // 처음 셋팅할 때 먼저 계산해서 값을 넣어주는게 좋음
+    CGSize result = CGSizeZero;
+    
+    result =  CGSizeMake(DEVICE_WIDTH, self.heightOfContentView);
     
     return result;
 }
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
-    NSMutableArray* elementsInRect = [NSMutableArray array];
+    NSLog(@"layoutAttributesForElementsInRect");
+    
+    return self.attributes;
+}
+
+
+- (NSArray *)makeAttributes
+{
+    NSMutableArray *result = [NSMutableArray array];
     
     for(NSInteger i = 0; i < [self.collectionView numberOfSections]; i++)
     {
         CGFloat originX = CELL_MARGIN;
         CGFloat originY = CELL_MARGIN;
-
+        
         for(NSInteger j = 0; j < [self.collectionView numberOfItemsInSection:i]; j++)
         {
             // 각 셀의 UICollectionViewLayoutAttributes에 위치할 frame 값을 계산해 넣어줌
@@ -64,7 +86,6 @@
                 
                 originX += (CELL_WIDTH + CELL_MARGIN);
                 
-                self.contentHeight = originY + CELL_WIDTH + CELL_MARGIN;
             }
             else if (index == COLUMN_COUNT - 1)
             {
@@ -73,32 +94,37 @@
                 originX = CELL_MARGIN;
                 originY += (CELL_WIDTH + CELL_MARGIN);
                 
-                self.contentHeight = originY;
             }
             
             NSLog(@"originX : %f, originY : %f", originX, originY);
-
-            if(CGRectIntersectsRect(cellFrame, rect))
-            {
-                NSIndexPath* indexPath = [NSIndexPath indexPathForRow:j inSection:i];
-                UICollectionViewLayoutAttributes* attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-                
-                attr.frame = cellFrame;
-                
-                [elementsInRect addObject:attr];
-            }
+            
+            NSIndexPath* indexPath = [NSIndexPath indexPathForRow:j inSection:i];
+            UICollectionViewLayoutAttributes* attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+            
+            attr.frame = cellFrame;
+            
+            [result addObject:attr];
         }
     }
     
-    return elementsInRect;
+    return result;
 }
 
-//- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    UICollectionViewLayoutAttributes *result = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-//    
-//    return result;
-//    
-//}
+- (CGFloat)calculateHeightOfContentView
+{
+    CGFloat result = 0.0f;
+    
+    NSInteger cellCount = [self.collectionView numberOfItemsInSection:0];
+    
+    NSInteger rowCount = cellCount / COLUMN_COUNT + (((cellCount % COLUMN_COUNT)!=0)?1:0);
+    
+    CGFloat contentHeight = rowCount * CELL_HEIGHT + ( rowCount + 1 ) * CELL_MARGIN;
+    
+    NSLog(@"contentHeight : %f", contentHeight);
+    
+    result = contentHeight;
+    
+    return result;
+}
 
 @end
